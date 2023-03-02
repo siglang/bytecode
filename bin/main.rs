15aadf242e910e205d607adbc7188b52ad90ffb9
ky0422 <ky0422a@gmail.com>
2023-03-02 12:47:55 +0900

    `update`: updated bin

--- bin/main.rs
+++ bin/main.rs
@@ -1,3 +1,24 @@
+use bytecode::{
+    opcode::{Op, OpData, Opcode},
+    vm::{Program, Vm},
+};
+
 fn main() {
-    
+    let opcodes = [
+        Op::new(Opcode::Push, Some(OpData::Value(0x00))),
+        Op::new(Opcode::JumpIfFalse, Some(OpData::Pointer(3))),
+        Op::new(Opcode::Jump, Some(OpData::Pointer(6))),
+        Op::new(Opcode::Push, Some(OpData::Value(0x46))),
+        Op::new(Opcode::PrintChar, None),
+        Op::new(Opcode::Jump, Some(OpData::Pointer(8))),
+        Op::new(Opcode::Push, Some(OpData::Value(0x54))),
+        Op::new(Opcode::PrintChar, None),
+        Op::new(Opcode::Noop, None),
+    ];
+    let program = Program(&opcodes);
+
+    println!("{program}");
+
+    Vm::new(program).run().unwrap();
+    println!();
 }
