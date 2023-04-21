a94a822408923e3a493d8e6db8c5df9a697f497c
ky0422 <ky0422a@gmail.com>
2023-04-21 19:53:13 +0900

    `update`: updated bin

--- bin/main.rs
+++ bin/main.rs
@@ -1,51 +1,38 @@
 use bytecode::{
-    opcode::{Op, OpcodeV1},
     rawbytes::RawBytes,
     vm::{Instructions, Program, Vm},
+    Code,
 };
 
-macro_rules! op {
-    ($opcode:ident) => {
-        Op::new(OpcodeV1::$opcode, None)
-    };
-    ($opcode:ident, $data:expr) => {
-        Op::new(OpcodeV1::$opcode, Some($data))
-    };
-}
-
 fn main() {
-    #[rustfmt::skip]
-    let opcodes = [
-        op!(Push, /* 0x01 */ 0x01 /* 0x01 0x00 */),
-        op!(JumpIfFalse, /* 0x08 */ 3  /* 0x02 3 */),
-        op!(Jump /* 0x07 */, 9 /* 0x02 9 */),
+    let test = r#"
+push 0x01
+jump_if_false 3
+jump 9
 
-        op!(Push, 0x65/* 0x01 0x65 */), // e
-        op!(Push, 0x73/* 0x01 0x73 */), // s
-        op!(Push, 0x6C/* 0x01 0x6C */), // l
-        op!(Push, 0x61/* 0x01 0x61 */), // a
-        op!(Push, 0x46/* 0x01 0x46 */), // F
+push 0x65
+push 0x73
+push 0x6C
+push 0x61
+push 0x46
 
-        op!(Debug /* 0xFF */),
+debug 0x00
 
-        op!(Push, 0x65/* 0x01 0x61 */), // e
-        op!(Push, 0x75/* 0x01 0x75 */), // u
-        op!(Push, 0x72/* 0x01 0x72 */), // r
-        op!(Push, 0x54/* 0x01 0x54 */), // T
+push 0x65
+push 0x75
+push 0x72
+push 0x54
 
-        op!(Debug /* 0xFF */),
-        op!(Noop /* 0x00 */),
-        // op!(Arguments /* 0x0C */),
-        // op!(Debug /* 0xFF */),
-    ];
+debug 0x00
+noop
+        "#;
 
-    let rawbytes: RawBytes = Instructions(&opcodes).into();
+    let rawbytes: RawBytes = Instructions(Code(test).parse().0).into();
     println!("{rawbytes:?}");
 
     let instructions: Instructions = rawbytes.try_into().unwrap();
 
     let program = Program(instructions);
-    println!("{program}");
 
     Vm::new(program).run().unwrap();
 }
