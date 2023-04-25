6629199065e0c752d9b1eabc36ced043e512de1d
ky0422 <ky0422a@gmail.com>
2023-04-25 18:06:32 +0900

    `update`: updated bin

--- bin/main.rs
+++ bin/main.rs
@@ -1,28 +1,77 @@
+use std::{fs, path::PathBuf};
+
 use bytecode::{
     rawbytes::RawBytes,
     vm::{Instructions, Program, Vm},
     Code,
 };
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(
+    bin_name = "bytecode",
+    version = "0.0.0",
+    arg_required_else_help = true
+)]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub subcommand: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum SubCommand {
+    #[clap(name = "b2r", about = "ByteCode source code file -> RawBytes file")]
+    B2R {
+        #[clap(short, long)]
+        input: PathBuf,
+        #[clap(short, long)]
+        output: PathBuf,
+    },
+    #[clap(name = "interpret", about = "Interpret RawBytes file")]
+    Interpret {
+        #[clap(short, long)]
+        input: PathBuf,
+    },
+    #[clap(name = "run", about = "Run ByteCode source code file")]
+    Run {
+        #[clap(short, long)]
+        input: PathBuf,
+    },
+}
 
 fn main() {
-    let test = r#"
-push 2 ;     0, stack: [ 2 ]
-push 3 ;     1, stack: [ 2, 3 ]
+    let args = Cli::parse();
 
-proc 2 ;     2: square, jump to `pc (= 2)` + `instruction length (= 2)` + `1 (proc)` (= `pointer 5`)
-    mul ;    3, stack: [ 6 ]
-    return ; 4, call stack pop, jump to `pointer 6` (StackFrame)
+    match args.subcommand {
+        SubCommand::B2R { input, output } => {
+            let bytecode = fs::read_to_string(input).unwrap();
+            let parsed = Code(&bytecode).parse().0;
+            let rawbytes: RawBytes = Instructions(parsed).into();
 
-call 3     ; 5, jump to `pointer 3`, call stack: [ StackFrame { pointer: 6 } ]
-debug 0    ; 6, stack: [ 6 ]
-        "#;
+            fs::write(output, rawbytes.0).unwrap();
+        }
+        SubCommand::Interpret { input } => {
+            let contents = fs::read(input).unwrap();
+            let rawbytes = RawBytes(&contents);
+            let instructions: Instructions = rawbytes.try_into().unwrap();
+            let program = Program(instructions);
 
-    let rawbytes: RawBytes = Instructions(Code(test).parse().0).into();
-    println!("{rawbytes:?}");
+            Vm::new(program).run().unwrap();
+        }
+        SubCommand::Run { input } => {
+            let bytecode = fs::read_to_string(input).unwrap();
+            let parsed = Code(&bytecode).parse().0;
+            let rawbytes: RawBytes = Instructions(parsed).into();
+            let instructions: Instructions = rawbytes.try_into().unwrap();
+            let program = Program(instructions);
 
-    let instructions: Instructions = rawbytes.try_into().unwrap();
+            Vm::new(program).run().unwrap();
+        }
+    }
 
-    let program = Program(instructions);
+    // let instructions: Instructions = rawbytes.try_into().unwrap();
 
-    Vm::new(program).run().unwrap();
+    // let program = Program(instructions);
+
+    // Vm::new(program).run().unwrap();
 }
